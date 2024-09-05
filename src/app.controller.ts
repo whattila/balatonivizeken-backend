ae938710cec2dce185bb3231d144327c31468726
whattila <ahhidati070@gmail.com>
2024-09-05 10:58:00 +0200

    SSE works. This is a trial and temporary state only. When it is over: delete index and sse methods from AppController, delete app.enableCors() from main.ts, and delete index.html

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Sse, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { AppService } from './app.service';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  index(@Res() response: Response) {
+    response
+      .type('text/html')
+      .send(readFileSync(join(__dirname, 'index.html')).toString());
+  }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map((_) => ({ data: { hello: 'world' } }) as MessageEvent),
+    );
   }
 }
